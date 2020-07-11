65a8fb43acb5dcc76779fedbf2e266200542ee4f
nduhiu17 <nduhiu254@gmail.com>
2020-07-11 23:14:03 +0300

    Updates

---
+++ precache-manifest.be1384b832da4bc766db54eb389d06bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cb4cd700d5a3312a212fcb1c24cd60",
+    "url": "/material-ui-react/index.html"
+  },
+  {
+    "revision": "8a66321084a0f0d502ba",
+    "url": "/material-ui-react/static/js/2.bd1633aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-ui-react/static/js/2.bd1633aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727660dbded794bfc2cf",
+    "url": "/material-ui-react/static/js/main.f1182cb3.chunk.js"
+  },
+  {
+    "revision": "33bf428019d254507900",
+    "url": "/material-ui-react/static/js/runtime-main.3fd7e5bb.js"
+  }
+]);
